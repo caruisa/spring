950c7f895b1ac3c1beb4b11b25c0b4f2c9f22d2d
caruisa <caruisa@naver.com>
2022-12-12 15:29:54 +0900

    GitHub Test

---
+++ missionrules/src/main/java/com/caruisa/missionrules/MissionrulesApplication.java
@@ -0,0 +1,13 @@
+package com.caruisa.missionrules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionrulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionrulesApplication.class, args);
+	}
+
+}
---
+++ missionrules/src/test/java/com/caruisa/missionrules/MissionrulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caruisa.missionrules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionrulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
