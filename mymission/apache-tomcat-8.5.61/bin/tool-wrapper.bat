50fd5e378b885ca347165ff2eecdfdf191bdbb7f
caruisa@naver.com <caruisa@naver.com>
2022-12-12 22:05:44 +0900

    abc

--- mymission/apache-tomcat-8.5.61/bin/tool-wrapper.bat
+++
@@ -1,40 +0,0 @@
-?ÇzyáN®öÒny…Ê¯àš#ïng´²™ÅdDBm
- n¨žv 2Ð¥xòj£ÂÝV)[ow ‰¿õPHì´›á=ø¼´[Ù`¢»£¼j¶öP¨9ž?!¹¬ "¼‚
-F)[Èµ²hÎBÝ‰öeéí—Ó5Œˆué™­ˆA_ðÄ¹2øë¡":®ÎÌû<é#¶25¢e3c˜|˜?	Æ.ÄîªAŠý>ˆ§·P§1/EU-Ôp|q­¡ÈXàÑ±IIÕ_–NÊB#Üßõ ¸·PÐxŒ\f&w¾,Aš-x—ÝßìvT¢b·×¹œ~#¦¦»ò¦¡†”ï}Ž(—{ÅÉNzùÄ0‰V|ÑÈÔÁôcõ`ÑVÔ¦›ÂÜ¼
-)Ý”Ú&õ±ò‚fMBÆä ƒSP ³P óÀDo¡É8¹ÖîÙ°‘ß×ðÞôÜ%â\/jº´3Š^ü†Þ‘+’unce¶25¢e3c˜|˜?ÈÔÁôcíYP ³P óÀobta3Š^a_–NÊáN®|˜?ÈÔÁôc ate3c˜e3c˜|||||http://www.aV|Ñ.org/ìvT¢b·×/LA_NSE-¼
-)Ý”e3c˜|UnlessÈquiré#¶by ap‘+’cabÞôÜlawÙ`¢µ²hÎBng´²™3Š^wrˆ§ng, s 2Ð¥xòe3c˜|¡":®ÎÌû<é#¶(—{ÅÉNzùÄ0ÈÔÁôc °‘ß¡":®ÎÌû<é#¶›ÂÜan "AS IS" BASIS,e3c˜|WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, e25¢ÅÉNexpressÈ`¢iÞ‘+’edf&w¾,A—Ó5Œˆué™ÈÔÁôc ªAŠˆué™specificÜlanguage governIIÕpermisÔ¦›ÂsÜand&w¾,Alimitp|q­¡s¶(—{ÅÉNzùÄ0ÈÔÁôcf&&w¾,A---------------------------------------------------------------------------e3c˜|Wrap‘ÅÉNscrijº´ªAŠü†ÞmandAlin¦¡ols
-)Ý”e3c˜|EnvironÝ‰öe VariabÞôÜPrequisitese3c˜e3c˜|||CATALINA_HOME|||M óÀpoiöe atƒSP r Catalina "build"ß¡"recy.e3c˜e3c˜|||TOOL_OPTS       (Op§·P§1) Java runtim[Ù`p§·Ps.e3c˜e3c˜|||JAVA_HOME|||||||MustÀpoiöe atƒSP r Java DevelopÝ‰öe Kiº´3Šstallp|q­¡.e3c˜|||||||||||||||||||UsIIÕJRE_HOME|3Šstead.ÄîsÜaÆ.ell.e3c˜e3c˜|||JRE_HOME||||||||MustÀpoiöe atƒSP r Java Runtim[Ù3Šstallp|q­¡.e3c˜|||||||||||||||||||Defaultsg´²™JAVA_HOME|if empty. If|JRE_HOME|andAJAVA_HOMEe3c˜|||||||||||||||||||¥xòjboth set,JRE_HOME|3sÉ8¹Öd.e3c˜e3c˜|||JAVA_OPTS       (Op§·P§1) Java runtim[Ù`p§·Ps.e3c˜e3c˜|||JAVA_ENDORSED_DIRS (Op§·P§1) L":sáN®|N®|semi-col›ÂÜsepara<é#¶¡"reciese3c˜|||||||||||||||||||9ž?!a3ŠIIÕsom[Ùjars™3Š^or{ÅÉNzo allowÈplacBÝ‰öe|N®|APIse3c˜|||||||||||||||||||9rea<é#¶outsideáN®|˜?JCP (i.DOM|andASAX fro˜|W3C).e3c˜|||||||||||||||||||It can also beÉ8¹ÖdNzo up)[o5Œˆué™XML parse¢iÞ‘+BÝ‰öep|q­¡.e3c˜|||||||||||||||||||TÙ°‘ß°‘ßonly suppor<é#¶ªAŠJava <= 8.e3c˜|||||||||||||||||||Defaultsg´²™$CATALINA_HOME/endor¹Öd.e3c˜A---------------------------------------------------------------------------e3
-setlocal&&w¾,AGuessÈCATALINA_HOME|if Do¡Édefin¦d
-set "CURRENT_DIR=%cd%"
-if Do¡É"%CATALINA_HOME%" == "" go´²™go´Hom[
-set "CATALINA_HOME=%CURRENT_DIR%"
-if ex":É"%CATALINA_HOME%\bin\¡ol-wrap‘ÅÉ.bat" go´²™okHom[
-cd ..
-set "CATALINA_HOME=%cd%"
-cd "%CURRENT_DIR%"
-:go´Hom[
-if ex":É"%CATALINA_HOME%\bin\¡ol-wrap‘ÅÉ.bat" go´²™okHom[
-Çzyáš-xCATALINA_HOME|environÝ‰öe variabÞôÜ°‘ßDo¡Édefin¦d|9žrrecly
-Çzyáš°‘ßenvironÝ‰öe variabÞôÜ°‘ßDÎBnÖdNzo runîÙ°‘ßprogra˜e3go´²™end&:okHom[
-e3c˜|Ensur5ŒˆuatƒanyÉ8¹ÖrÉdefin¦d|CLASSPATH variabÞôs|¥xòjDo¡É8¹Ö#¶›ÂÜstartup,e3c˜|Ìû< allowÈˆuémNzo bé™specifiÖ#¶iÂÜsetenv.bat,¶iÂÜr¥xòjcac when iº´3‘ßDÎBnÖd.
-set CLASSPATH=&&w¾,AGet staV)[rdßenvironÝ‰öe variabÞôs
-if ex":É"%CATALINA_HOME%\bin\setenv.bat"jcallÉ"%CATALINA_HOME%\bin\setenv.bat"&&w¾,AGet staV)[rdßJava environÝ‰öe variabÞôs
-if ex":É"%CATALINA_HOME%\bin\setclasspath.bat" go´²™okSetclasspath
-ÇzyáCanDo¡ÉfindÉ"%CATALINA_HOME%\bin\setclasspath.bat"
-Çzyáš°‘ß×ðÞôÜ°‘ßDÎBnÖdNzo runîÙ°‘ßprogra˜e3go´²™end&:okSetclasspath
-callÉ"%CATALINA_HOME%\bin\setclasspath.bat" %1
-if errorlevel 1 go´²™end&&w¾,AAd#¶›ÂÜextraÙjarß×ðÞôsg´²™CLASSPATH&w¾,ANot5Œˆuatƒ5¢ÅÉe|¥xòjDo quot5sÜaÆ.e dojDo¡Éwaöe ´²™3Štroduce¶raV)o˜e3c˜|quot5sÜ3Št²™ÅdDBCLASSPATH&if "%CLASSPATH%" == "" go´²™emptyClasspath
-set "CLASSPATH=%CLASSPATH%;"
-:emptyClasspath
-set "CLASSPATH=%CLASSPATH%%CATALINA_HOME%\bin\boot:®ap.jar;%CATALINA_HOME%\bin\¡mcat-juli.jar;%CATALINA_HOME%\lib\servlet-api.jar;%CATALINA_HOME%\lib\¡mcat-util.jar"e3
-set|JAVA_OPTS=%JAVA_OPTS% -Djava.util.loggIIÕ.manager=org.aV|Ñ.juli.ClassLoa{ÅÉLogManager&&w¾,AJava 9jDo l›ÂgÅÉNsuppor<s™ÅdDBjava.endor¹Öd.¡"rse3c˜|systemßpro‘ÅÉty. Only tryNzo uc °º´3öÒny…JAVA_ENDORSED_DIRS waÆ.exp+’citly sete3c˜È`¢CATALINA_HOME/endor¹Öd ex":s.
-set ENDORSED_PROP=ign£¼.endor¹Öd.¡"rse3if "%JAVA_ENDORSED_DIRS%" == "" go´²™noEndor¹ÖdVar
-set ENDORSED_PROP=java.endor¹Öd.¡"rse3go´²™d¼´[Endor¹Öd
-:noEndor¹ÖdVar
-if Do¡Éex":É"%CATALINA_HOME%\endor¹Öd" go´²™d¼´[Endor¹Öd
-set ENDORSED_PROP=java.endor¹Öd.¡"rse3:d¼´[Endor¹Öd
-&w¾,AGet w¾,a3ŠIIÕunxŒ\ft¦d|9žÞmandAlin¦arguÝ‰öeé|andAsaveÈˆuémNinîÙ[
-set CMD_LINE_ARGS=&:setArgse3if ""%1""=="""" go´²™d¼´[SetArgse3set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
-xŒ\fte3go´²™setArgse3:d¼´[SetArgse3
-%_RUNJAVA% %JAVA_OPTS% %TOOL_OPTS% -D%ENDORSED_PROP%="%JAVA_ENDORSED_DIRS%" -classpath "%CLASSPATH%" -Dcatalina.hom[="%CATALINA_HOME%" org.aV|Ñ.catalina.startup.T¡ol %CMD_LINE_ARGS%e3
-:end&
