094188388a40fd893bb746b2babd29540a9e76f6
caruisa <caruisa@naver.com>
2022-12-13 15:23:01 +0900

    자바 놀이터 commit go!~

---
+++ JavaPlayground/src/main/java/com/caruisa/mission/MissionController2.java
@@ -0,0 +1,39 @@
+package com.caruisa.mission;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class MissionController2 {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MissionController2.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/mission2", method = RequestMethod.GET)
+	public String mission2(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "mission2";
+	}
+	
+}
---
+++ JavaPlayground/src/main/webapp/WEB-INF/views/mission2.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	낭만 놀이터
+</h1>
+<div><a href="">홈</a></div>
+<div><a href="mission1">mission1</a></div>
+<P>  The time on the server is ${serverTime}. </P>
+</body>
+</html>
